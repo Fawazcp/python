89620832700f082a832bf56ed11750b48d74e6f1
Fawaz C P <111639918+Fawazcp@users.noreply.github.com>
2023-07-12 15:39:56 +0530

    Create add-user.py

---
+++ add-user.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+
+import os
+userlist=["fawaz","aws","python"]
+
+print("Adding users to the script")
+print("############################")
+
+# loop to add users from userlist
+for user in userlist:
+    exitcode=os.system(f"id {user}")
+    if exitcode != 0:
+        print(f"User {user} does not exist. Adding it")
+        print("###########################################")
+        print()
+        os.system(f"useradd {user}")
+    else:
+        print("User already exist, skipping it")
+        print("#################################")
+        print()
+
